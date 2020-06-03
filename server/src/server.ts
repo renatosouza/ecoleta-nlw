fe39514ff783d4ee0cbf3ccec20544567ae8ad39
Renato <renatosouzavv@gmail.com>
2020-06-03 03:42:30 -0300

    Added connection and migrations to database (using sqlite)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,10 @@
 import express from 'express';
+import routes from './routes';
+
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
-
-    response.json([
-        'Renato',
-        'Felipe',
-        'Valeska',
-        'Junin'
-    ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
